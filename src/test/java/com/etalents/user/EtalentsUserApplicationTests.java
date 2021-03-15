074982ad7dc92031da12554b0e6863c507355c5f
Felipe Carreira <felipe.carreira@hotmail.com>
2021-03-15 15:25:40 -0300

    feature: Start Project

---
+++ src/test/java/com/etalents/user/EtalentsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etalents.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtalentsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
